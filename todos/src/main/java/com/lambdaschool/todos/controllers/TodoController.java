517fded34aeb0eeb9e3d8d33f748335b72131ff2
allisonkydy <allisnelly@gmail.com>
2019-10-11 10:48:33 -0500

    create todo controller, connect to service, add updatetodo endpoint

---
+++ todos/src/main/java/com/lambdaschool/todos/controllers/TodoController.java
@@ -0,0 +1,27 @@
+package com.lambdaschool.todos.controllers;
+
+import com.lambdaschool.todos.models.Todo;
+import com.lambdaschool.todos.services.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/todos")
+public class TodoController
+{
+  @Autowired
+  private TodoService todoService;
+
+  // PUT -- http://localhost:2019/todos/todoid/{todoid}
+  // updates a to-do based on given todoid
+  @PutMapping(value = "/todoid/{todoid}",
+              consumes = {"application/json"})
+  public ResponseEntity<?> updateTodo(@RequestBody Todo todo,
+                                      @PathVariable long todoid)
+  {
+    todoService.updateTodo(todo, todoid);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+}
